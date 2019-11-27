3246707852aa89e6a2733b1f9dc8ae0fe9aae90b
Ethan Oblak <efunn@github.com>
2019-11-26 18:10:21 -0600

    sample RSA analysis

--- analysis.py
+++ analysis.py
@@ -53,3 +53,46 @@ def gen_hrf(tr=2, n_trs=15, c=1./6, a1=6, a2=16):
     h = (np.exp(-t)*(t**(a1-1)/np.math.factorial(a1-1)
          - c*t**(a2-1)/np.math.factorial(a2-1)))
     return h/np.sum(h)
+
+
+################
+# RSA analysis #
+################
+import matplotlib.pyplot as plt
+import seaborn as sea
+import matplotlib.gridspec as gridspec
+
+num_fingers = 4
+trials_per_finger = 40
+gs = gridspec.GridSpec(2,6)
+axs = [plt.subplot(gs[0]),plt.subplot(gs[1]),plt.subplot(gs[2]),plt.subplot(gs[3]),plt.subplot(gs[4]),plt.subplot(gs[5]),
+       plt.subplot(gs[6]),plt.subplot(gs[7]),plt.subplot(gs[8]),plt.subplot(gs[9]),plt.subplot(gs[10]),plt.subplot(gs[11])]
+plt.ion()
+all_mats = np.zeros((num_fingers,num_fingers,len(SUBJ_IDS)))
+for idx,subj_id in enumerate(SUBJ_IDS):
+    fmri_data = moving_average(np.load('data/'+subj_id+'_localizer.npy'))[TRS_TO_EXTRACT,:]
+    press_data = pd.read_csv('data/'+subj_id+'_localizer_press.csv')
+    trial_data = press_data.groupby('trial',sort=False).first() 
+    trial_labels = trial_data.target_finger
+    sorted_labels=trial_labels.sort_values()
+    sorted_fmri_data=fmri_data[sorted_labels.index,:]
+    corr_mat = np.arctanh(np.corrcoef(sorted_fmri_data))
+    np.fill_diagonal(corr_mat,0)
+    mini_mat = np.zeros((num_fingers,num_fingers))
+    for diag_element in range(num_fingers):
+        base_pos = diag_element*trials_per_finger
+        final_pos = (diag_element+1)*trials_per_finger
+        mini_mat[diag_element,diag_element]=np.mean(
+            corr_mat[base_pos:final_pos,base_pos:final_pos][np.tril_indices(trials_per_finger,-1)])
+    for off_diag in zip(np.tril_indices(num_fingers,-1)[0],np.tril_indices(num_fingers,-1)[1]):
+        x_base_pos = off_diag[0]*trials_per_finger
+        x_final_pos = (off_diag[0]+1)*trials_per_finger
+        y_base_pos = off_diag[1]*trials_per_finger
+        y_final_pos = (off_diag[1]+1)*trials_per_finger
+        mini_mat[off_diag] = np.mean(corr_mat[x_base_pos:x_final_pos,y_base_pos:y_final_pos])
+    all_mats[:,:,idx] = mini_mat
+    if idx == len(SUBJ_IDS)-1:
+        cbar_bool = True
+    else:
+        cbar_bool = False
+    sea.heatmap(mini_mat,ax=axs[idx],vmin=-0.25,vmax=0.25,square=True,cbar=cbar_bool,cmap='coolwarm')
